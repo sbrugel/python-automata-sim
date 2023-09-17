6e41cc6ebdaf916384fbfab1845cf9617883a8de
sbrugel <sbrugel62@gmail.com>
2023-09-16 23:14:32 -0400

    complete

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Finite State Machine Simulator
 
-TBA
+This is a CLI simulation of a finite state machine made in Python. Made to scratch an itch to program in Python, and preparation for a side project in React I will be developing with a friend of mine, I intended this to be a FSM validator for homework assignments for CISC303, Automata Theory.
+
+Currently this only supports the alphabet (a, b) for the automated test cases. I have code which reads input for the FSM's alphabet as comma-separated input commented out, but if it's not just those two letters you will need to write your own test cases.
+
+This was inspired by Dr. Silber's fantastic [finite state machine simulation](https://www.eecis.udel.edu/~silber/automata). I took the idea of that application and added in a few extra things, including:
+- Automatic testing based on a number of test cases (supporting only the alphabet {a, b} for now)
+- Checks if every state has transitions for all letters *(this may be removed)* and checks for duplicate transitions
+- Faster running per test case (you don't have to click next for each letter, it just runs the whole thing right away)
+
+The features here are subject to change depending on what we learn in CISC303
+
+## How to use
+I would recommend using Dr. Silber's automata simulation to construct the machine beforehand, you can put the inputs as specified below in a text file then paste it into your terminal. (His simulation contains a table with all edges in the FSM)
+
+1. Input the number of states (must be >= 2). If you create 2 states, it will create states q0 (with ID 0) and q1 (with ID 1)
+2. Input the ID of the starting state (zero indexed)
+3. Input the ID(s) of accepting states (zero indexed)
+4. Add in transitions in this format: LETTER, FROM_STATE_ID, TO_STATE_ID. Keep entering them until you are satisfied, then hit Return with an empty input
+5. Sample output for a simple FSM is included in this repository. This returns if each string test case is in the language, and how the simulation determined that. *(a q0-q1) means the test took the transition for letter 'a' between q0 and q1.*
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os, sys
+
 class State:
     def __init__(self, name: str, accepting: bool) -> None:
         self.name = name
@@ -22,6 +24,26 @@ class FSM:
         self.start_state = start_state
         self.transitions = transitions
 
+    """
+    This returns True if every state in the FSM has a transition from it for every letter in the alphabet provided.
+    """
+    def states_have_all_transitions(self) -> bool:
+        for state in self.states:
+            trans_from_state = [trans for trans in self.transitions if trans.start == state]
+            if len(trans_from_state) < len(self.alphabet):
+                return False
+        return True
+            
+    """
+    This returns the transition with the given letter and start state. If it doesn't exist, it returns None.
+    """
+    def find_transition(self, letter: str, start: State) -> Transition:
+        for trans in self.transitions:
+            if trans.letter == letter and trans.start == start:
+                return trans
+            
+        return None
+
     def __str__(self) -> str:
         ret_str = 'FSM Data:'
         ret_str += '\n\tAlphabet: ' + ', '.join(self.alphabet)
@@ -29,16 +51,23 @@ class FSM:
         for state in self.states:
             ret_str += '\n\t\t' + str(state)
         ret_str += '\n\tStarting State:\n\t\t' + str(self.start_state)
-        ret_str += '\n\tAccepting States:'
-        for state in self.states:
-            if state.accepting:
-                ret_str += '\n\t\t' + str(state)
         ret_str += '\n\tTransitions:'
         for trans in self.transitions:
             ret_str += '\n\t\t' + str(trans)
-
         return ret_str
+    
+"""
+Clears the terminal.
+"""
+def os_agnostic_clear():
+    if sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
 
+"""
+Gets the state with the given ID (starts from 0).
+"""
 def find_state_by_id(states: [State], id: int) -> State:
     for state in states:
         if state.name == 'q' + str(id):
@@ -47,23 +76,29 @@ def find_state_by_id(states: [State], id: int) -> State:
     return None
 
 def main():
-    fsm = FSM([], [], None, [])
+    os_agnostic_clear()
+    fsm = FSM(['a', 'b'], [], None, [])
 
-    alphabet = input('Enter letters of the alphabet separated by commas: ')
-    while len(alphabet) == 1 and alphabet[0] == '':
-        alphabet = input('Enter letters of the alphabet separated by commas: ')
-    fsm.alphabet = alphabet.strip().replace(' ', '').split(',')
+    # alphabet = input('Enter letters of the alphabet separated by commas: ').strip().replace(' ', '').split(',')
+    # invalid_alphabet = any(len(alpha) > 1 for alpha in alphabet) # letters cannot be more than one char long
+    # while (len(alphabet) == 1 and alphabet[0] == '') or invalid_alphabet:
+    #     alphabet = input('Enter letters of the alphabet separated by commas: ').strip().replace(' ', '').split(',')
+    #     invalid_alphabet = any(len(alpha) > 1 for alpha in alphabet)
+    # fsm.alphabet = alphabet
 
-    while True:
+    os_agnostic_clear()
+    while True: # keep reading inputs until we get a num >= 2
         try:
-            num_states = int(input('How many states? '))
+            num_states = int(input('How many states? (at least 2) '))
             max_state_id = num_states - 1
-            break
+            if num_states >= 2:
+                break
         except ValueError:
             print('')
 
+    os_agnostic_clear()
     start_state = None
-    while True:
+    while True: # keep reading inputs until we get a num between 0 and max state id
         try:
             start_state = int(input('Enter the ID of the start state, between 0 and ' + str(max_state_id) + ': '))
             if not (start_state < 0 or start_state > max_state_id):
@@ -71,14 +106,24 @@ def main():
         except ValueError:
             print('')
 
-    accepting_states = input('Enter the IDs of the accepting states separated by commas between 0 and ' + str(max_state_id) + ': ').strip().replace(' ', '').split(',')
-    while len(accepting_states) == 1 and accepting_states[0] == '':
+    os_agnostic_clear()
+
+    while True: # keep reading inputs until we get a valid input (comma separated of existing state IDs)
         accepting_states = input('Enter the IDs of the accepting states separated by commas between 0 and ' + str(max_state_id) + ': ').strip().replace(' ', '').split(',')
+        try:
+            any_invalid_states = any((int(id_given) > max_state_id or int(id_given) < 0) for id_given in accepting_states)
+            if any_invalid_states or (len(accepting_states) == 1 and accepting_states[0] == ''):
+                continue
+        except ValueError:
+            continue
+        break
     
-    for i in range(num_states):
+    for i in range(num_states): # create all the states
         this_state = State('q' + str(i), str(i) in accepting_states)
         fsm.states.append(this_state)
     fsm.start_state = find_state_by_id(fsm.states, int(start_state))
+
+    os_agnostic_clear()
 
     while True:
         print('Current Transitions:')
@@ -86,77 +131,110 @@ def main():
             print('\t' + str(trans))
         print('State IDs range from 0 to', max_state_id)
         trans = input('Enter a transition in this format: \'LETTER, FROM_STATE_ID, TO_STATE_ID\'. Enter nothing to stop: ').strip().replace(' ', '').split(',')
+
         if len(trans) == 1 and trans[0] == '' and not len(fsm.transitions) == 0:
-            # TODO
-            # Check every state to see if they have an edge for all letters
-            # If there is one without, print error and try again
+            if not fsm.states_have_all_transitions(): # only reached when trying to submit
+                os_agnostic_clear()
+                print('ERROR: Not all states have transitions for every letter! Clearing transitions...')
+                fsm.transitions = []
+                continue
+
+            # clear duplicate transitions
+            fsm.transitions = list(set(fsm.transitions))
             break
 
-        if not len(trans) == 3:
+        if not len(trans) == 3: # 
+            os_agnostic_clear()
+            print('ERROR: Submit 3 items for each transition')
             continue
 
         valid_letter = trans[0] in fsm.alphabet
         valid_from = 0 <= int(trans[1]) <= max_state_id
         valid_to = 0 <= int(trans[2]) <= max_state_id
         if not valid_letter or not valid_from or not valid_to:
+            os_agnostic_clear()
+            print('ERROR: One of your inputs was invalid (i.e. letter not in alphabet, invalid state ID)')
             continue
-        # TODO: check for duplicate transitions (use sets!)
+
+        if fsm.find_transition(trans[0], find_state_by_id(fsm.states, int(trans[1]))):
+            os_agnostic_clear()
+            print('ERROR: A transition for this start state with this letter already exists and was not added')
+            continue
 
         new_trans = Transition(trans[0], find_state_by_id(fsm.states, int(trans[1])), find_state_by_id(fsm.states, int(trans[2])))
         fsm.transitions.append(new_trans)
+        os_agnostic_clear()
 
+    os_agnostic_clear()
     print(fsm)
 
-    str_test_cases = ['', 'a', 'b', 'aa', 'bb']
-    for test in str_test_cases:
-        pass
-    
-    # TODO
-    # Init array of test cases
-    # For each test case
-        # Start with a pointer which points to starting state of FSM
-        # Find the edge corresponding to the current letter, starting from this state
-        # Set pointer to the to state of this transition
-        # Remove the first char of the string
-        # If string == ''
-            # If pointer is on an accepting state return True, else False
+    str_test_cases = [
+        "", # empty string
+        "a", # single chars
+        "b",
+        "aa", # pairs of chars
+        "bb",
+        "ab",
+        "ba",
+        "aba", # trios of chars
+        "bab",
+        "abb",
+        "baa",
+        "aab",
+        "bba",
+        "aaa",
+        "bbb",
+        "aaaa", # 4 chars
+        "bbbb",
+        "abab",
+        "aaab",
+        "abbb",
+        "abba",
+        "baba",
+        "baaa",
+        "bbba",
+        "baab",
+        "aabb",
+        "bbaa",
+        "ababbbaaabba", # long strings of even len
+        "ababbbaaabbb",
+        "abaaaabbbaaa",
+        "abaaaabbbaab",
+        "abbbbbbbbbbb",
+        "aabbbbbbbbbb",
+        "bbaaaaaaaaaa",
+        "baaaaaaaaaaa",
+        "bbaaaaaaaaab",
+        "abbbbbbbbbba",
+        "ababbbaaaba", # long strings of odd len
+        "ababbbaaabb",
+        "abaaaabbbaa",
+        "abaaaabbbab",
+        "abbbbbbbbbb",
+        "aabbbbbbbbb",
+        "bbaaaaaaaaa",
+        "baaaaaaaaaa",
+        "bbaaaaaaaab",
+        "abbbbbbbbba",
+        "aababb", # other random stuff
+        "bbabaa"
+    ]
+
+    for testing_string in str_test_cases:
+        orig_str = testing_string # for printing purposes
+        state_pointer = fsm.start_state # where are we at in the FSM?
+        path = [] # list of tuples showing where the simulation went
+        while not testing_string == '':
+            current_char = testing_string[0]
+            path.append((current_char, state_pointer.name, fsm.find_transition(current_char, state_pointer).end.name))
+            state_pointer = fsm.find_transition(current_char, state_pointer).end # move to the next state
+            testing_string = testing_string[1:] # remove first char
+
+        if state_pointer.accepting:
+            print('\033[92mThe string', (orig_str if not orig_str == '' else 'EMPTY STRING'), 'is in the language\033[0m')
+        else:
+            print('\033[91mThe string', (orig_str if not orig_str == '' else 'EMPTY STRING'), 'is NOT in the language\033[0m')
+        tuple_to_str = ", ".join([f"({str(item[0])} {str(item[1])}-{str(item[2])})" for item in path])
+        print('\t' + tuple_to_str)
 
 main()
-
-"""
-
-a
-b
-aa
-bb
-ab
-ba
-aba
-bab
-abb
-baa
-aab
-bba
-aaa
-bbb
-aaaa
-bbbb
-abab
-aaab
-abbb
-abba
-baba
-baaa
-bbba
-baab
-ababbbaaabba
-ababbbaaabbb
-abaaaabbbaaa
-abaaaabbbaaa
-abbbbbbbbbbb
-aabbbbbbbbbb
-bbaaaaaaaaaa
-baaaaaaaaaaa
-bbaaaaaaaaab
-abbbbbbbbbba
-"""
